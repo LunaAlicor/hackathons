1f485880ae394d97aba278893bb8194d90004efc
LunaAlicor <84154835+LunaAlicor@users.noreply.github.com>
2023-09-30 23:54:51 +0300

    1

--- Service for hackathons/hackathon/main/admin.py
+++ Service for hackathons/hackathon/main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import News, Event, Comment, Tag, Team
+from .models import News, Event, Comment, Tag, Team, Membership
 
 admin.site.register(News)
 admin.site.register(Event)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Team)
+admin.site.register(Membership)
 
 # Register your models here.
